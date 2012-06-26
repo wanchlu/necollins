435d4cb585cdbab536148210941b0051f4be2a59
Wanchen Lu <luwanchen@gmail.com>
2012-06-25 23:52:45 -0400

    j

--- DLCoTrain/src/DLCoTrain.java
+++ DLCoTrain/src/DLCoTrain.java
@@ -1,20 +1,82 @@
 import java.util.List;
+import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.ArrayList;
 
-import java.io.File;
+//import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+class Global {
+	public static final double pmin = 0.95;
+}
+class CountHash {
+	private Map<String,Integer> LCount= new HashMap<String,Integer>();	// = Count(key,L)
+	private Map<String,Integer> PCount= new HashMap<String,Integer>(); // = Count(key,P)
+	private Map<String,Integer> OCount= new HashMap<String,Integer>();	// = Count(key,O)
+	private Map<String,Integer> TCount= new HashMap<String,Integer>(); // = Count(key)
+	public CountHash (TrainSet trainSet) {
+		 for (TrainExample e: trainSet.getLabeledExamples()) {
+			 for (String f: e.getFeatures()) {
+				 LCount.put(f, 0);
+				 PCount.put(f, 0);
+				 OCount.put(f, 0);
+				 TCount.put(f, 0);
+			 }
+		 }
+		 for (TrainExample e: trainSet.getLabeledExamples()) {
+			 Type t = e.getType();
+			 for (String f: e.getFeatures()) {
+				 TCount.put(f, TCount.get(f) + 1);
+				 switch (t) {
+				 	case L:	LCount.put(f, LCount.get(f) + 1); 
+				 		break;
+				 	case P: PCount.put(f, PCount.get(f) + 1); 
+				 		break;
+				 	case O: OCount.put(f, OCount.get(f) + 1); 
+				 		break;
+				 	default: break;
+				 }
+			 }
+		 }
+	}
+	public Integer getTypeCount (Type t, String f) {
+		switch (t) {
+		case L:	return LCount.get(f);
+		case P: return PCount.get(f);
+		case O: return OCount.get(f);
+		default: return 0; 	
+		}
+	}
+	public Integer getTotalCount (String f) {
+		return TCount.get(f);
+	}
+	public Iterator getTypeCountIterator (Type t) {
+		switch (t) {
+		case L: return LCount.entrySet().iterator();
+		case P: return PCount.entrySet().iterator();
+		case O: return OCount.entrySet().iterator();
+		default: return TCount.entrySet().iterator();
+		} 
+	}
+	public Iterator getTotalCountIterator() {
+		return  TCount.entrySet().iterator();
+	}
+}
 
 class TestExample {
 	
 }
 class TrainSet {
 	private List<TrainExample> trainexamples;
+	private List<TrainExample> labeledexamples;
 	public TrainSet (String filename) {
 		trainexamples = new ArrayList<TrainExample> ();
+		labeledexamples = new ArrayList<TrainExample> ();
 		BufferedReader trainFile = null;
 		try {
 			trainFile = new BufferedReader(new FileReader (filename));
@@ -23,7 +85,7 @@ class TrainSet {
             System.exit(1);
         }
 		try {
-			Integer exampleid = 0;
+			int exampleid = 0;
 			while (true) {
 				String exampleLine = trainFile.readLine();
 				if (exampleLine == null)
@@ -38,33 +100,65 @@ class TrainSet {
             System.exit(1);
         }	
 	}
+	public List<TrainExample> getLabeledExamples () {
+		return labeledexamples;
+	}
+	//return the iith example
+	public TrainExample getTrainExample (int ii) {
+		return trainexamples.get(ii);
+	}
 	// return how many examples got labeled
-	public Integer LabelUsingDL (DecisionList dl) {
-		Integer count = 0;
-		for (int i = 0; i < trainexamples.size(); i++) {
-			for (int j = 0; j < dl.size(); j++) {
+	public int LabelUsingDL (DecisionList dl) {
+		int count = 0;
+		labeledexamples = new ArrayList<TrainExample> ();		
+		for (int i = 0; i < trainexamples.size(); i++) {	// for every example
+			Boolean labeled = false;
+			for (int j = 0; j < dl.size(); j++) {	// for every rule
 				Rule rule = dl.getRule(j);
-				
+				for (String f:this.getTrainExample(i).getFeatures()){	// for every feature of the example
+					if (f.equals(rule.getFeature()) ) {	// feature exists in a rule
+						getTrainExample(i).setType(rule.getType());	// label the example
+						labeledexamples.add(this.getTrainExample(i));
+						labeled = true;
+						count ++;
+		/*				System.out.print("labeling ");
+						getTrainExample(i).print();
+						System.out.print("using rule ");
+						rule.print();						
+			*/		}
+					if (labeled) break;
+				}
+				if (labeled) break;
 			}
 		}
+		return count;
 	}
 }
 class TrainExample {
 	private Type type = null;
 	private List<String> features;
-	private Integer id;
-	public TrainExample (Integer ii, String str) {
+	private String featureString;
+	private int id;
+	public TrainExample (int ii, String str) {
 		id = ii;
 		features = new ArrayList<String> ();
 		String[] tokens = str.split("\\s+");	
 		for (String t:tokens) {
 			features.add(t);
 		}
+		featureString = str;
 	}
-	public Integer getId () { return id; }
+	public int getId () { return id; }
 	public List<String> getFeatures () { return features; }
+	public String getFeature (int ii) {
+		return features.get(ii);
+	}
 	public void setType (String s) { type = Type.valueOf(s); }
+	public void setType (Type t) { type = t; }
 	public Type getType () { return type; }
+	public void print () {
+		System.out.println("TrainExample "+id+": "+type+" "+featureString);
+	}
 }
 
 class Rule {
@@ -87,6 +181,9 @@ class Rule {
 
 class DecisionList {
 	private List<Rule> rulelist;
+	public DecisionList () {
+		rulelist = new ArrayList<Rule> ();		
+	}
 	public DecisionList (String filename) {
 		rulelist = new ArrayList<Rule> ();
 		BufferedReader seedFile = null;
@@ -97,7 +194,6 @@ class DecisionList {
             System.exit(1);
         }
 		try {
-			//seedFile.readLine();
 			while (true) {
 				String seedLine = seedFile.readLine();
 				if (seedLine == null)
@@ -111,16 +207,25 @@ class DecisionList {
             System.exit(1);
         }	
 	}
-	public Rule getRule (Integer ii) {
+	public Rule getRule (int ii) {
 		return rulelist.get(ii);
 	}
-	public Integer size () {
+	public int size () {
 		return rulelist.size();
 	}
 	public void print () {
 		for (int i = 0; i < rulelist.size(); i++) {
 			rulelist.get(i).print();
 		}
+	}
+	// return number of new induced rules, add to the original rulelist
+	public int induceUsingLabeledSet (int n, CountHash countHash) {
+		int count = 0;
+		Iterator Liter = countHash.getTypeCountIterator(Type.valueOf("L"));
+		Iterator Piter = countHash.getTypeCountIterator(Type.valueOf("P"));
+		Iterator Oiter = countHash.getTypeCountIterator(Type.valueOf("O"));
+		Iterator Titer = countHash.getTotalCountIterator();
+		return count;
 	}
 }
 
@@ -131,7 +236,7 @@ public class DLCoTrain {
 	 */
 	public static void main(String[] args) {
 		// 1. set n= = 5
-		Integer n = 5;
+		int n = 5;
 		// 2. Initialization
 		DecisionList spellingDL = new DecisionList(
 				"/Users/wanchen/github/necollins/DLCoTrain/necollinssinger/all.seed.rules");
@@ -139,7 +244,12 @@ public class DLCoTrain {
 		TrainSet trainSet = new TrainSet (
 				"/Users/wanchen/github/necollins/DLCoTrain/necollinssinger/all.train.ex");
 		// 3. Label the training set using the current set of spelling rules
-		
+		System.out.println("\nTotally labeled "+trainSet.LabelUsingDL(spellingDL)+" examples");
+		// 4. Use labeled examples to induce contextual DL
+		DecisionList contextualDL = new DecisionList();
+		CountHash countHash = new CountHash (trainSet);
+		//System.out.println(countHash.getTypeCount(Type.valueOf("P"), "X2_Mr.")+" "+countHash.getTotalCount( "X2_Mr."));
+		contextualDL.induceUsingLabeledSet(n, countHash);
 	}
 
 }
--- necollinssinger/z.train.ex
+++
@@ -1,3 +0,0 @@
-X2_Mr X2_president X2_Smith
-X2_president X2_John
-X2_Smith X2_Inc
